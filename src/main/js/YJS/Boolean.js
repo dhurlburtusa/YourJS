a790bae5329c1cfb92ce66ea594a00441c3eecd2
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2015-06-26 20:52:07 -0600

    Added YJS.Boolean.convert

--- src/main/js/YJS/Boolean.js
+++ src/main/js/YJS/Boolean.js
@@ -7,6 +7,8 @@
  * @singleton
  * @class YJS.Boolean
  * A set of boolean related methods.
+ * 
+ * @uses YJS.Utils
  */
 (function (YJS, nsPath) {
 
@@ -18,7 +20,92 @@ var NS = YJS.ns(nsPath),
     YJS_Boolean;
 
 NS.Boolean = YJS_Boolean = {
-    $LOG: YJS.__.tmp.LOG
+    $LOG: YJS.__.tmp.LOG,
+    __ : {
+        lut: {
+            '0': false,
+            '1': true,
+            'f': false,
+            't': true,
+            'n': false,
+            'y': true,
+            'on': true,
+            'off': false,
+            'no': false,
+            'yes': true,
+            'false': false,
+            'true': true
+        }
+    }
+};
+
+// ==========================================================================
+/**
+ * Attempts to convert given input to a boolean (primitive).
+ * 
+ * The following values return `false`:
+ * 
+ * - `0`
+ * - `false`
+ * - `'0'`
+ * - `'f'`
+ * - `'F'`
+ * - `'false'` and any case variation like `'False'`.
+ * - `'n'`
+ * - `'N'`
+ * - `'no'` and any case variation like `'No'`.
+ * - `'off'` and any case variation like `'Off'`.
+ * 
+ * The following values return `true`:
+ * 
+ * - `1`
+ * - `true`
+ * - `'1'`
+ * - `'on'` and any case variation like `'On'`.
+ * - `'t'`
+ * - `'T'`
+ * - `'true'` and any case variation like `'True'`.
+ * - `'y'`
+ * - `'Y'`
+ * - `'yes'` and any case variation like `'Yes'`.
+ * 
+ * All other values return the default value (`null` by default).
+ * 
+ * NOTE: This function behaves differently than the native `Boolean` constructor function.
+ * 
+ *     YJS.Boolean.convert(true); // true
+ *     YJS.Boolean.convert('Yes'); // true
+ *     YJS.Boolean.convert('true'); // true
+ *     YJS.Boolean.convert(false); // false
+ *     YJS.Boolean.convert('No'); // false
+ *     YJS.Boolean.convert('false'); // false
+ * 
+ * See the Jasmine Specs for more example uses.
+ * 
+ * @param {?Mixed} input The value to be converted to a boolean.
+ * @param {Object} [options] The options to use when doing the conversion.
+ * @param {Mixed} [options.def=null] The default value to return if unable to convert.
+ * 
+ * @return {boolean|null} The input converted to a boolean or the default value if unable to convert.
+ */
+YJS_Boolean.convert = function (input, options) {
+    var defValue, output, typeOfInput;
+
+    options = options || {};
+    defValue = options.hasOwnProperty('def') ? options.def : null;
+    output = defValue;
+    typeOfInput = YJS.Utils.typeOf(input);
+    if (typeOfInput == 'boolean') {
+        output = input;
+    } else if (typeOfInput != 'undefined' && typeOfInput != 'null' && typeof input.toString == 'function') {
+        input = input.toString().toLowerCase();
+        // The following check ensures only a boolean is returned.
+        if (YJS.Boolean.__.lut.hasOwnProperty(input)) {
+            output = YJS.Boolean.__.lut[input];
+        }
+    }
+
+    return output;
 };
 
 // ==========================================================================
--- src/test/js/unit/spec/YJS/BooleanSpec.js
+++ src/test/js/unit/spec/YJS/BooleanSpec.js
@@ -10,6 +10,265 @@
  */
 describe("YJS.Boolean", function () {
 
+    describe(".convert", function () {
+
+        it("called with `true` should return `true`", function () {
+            expect(YJS.Boolean.convert(true)).toBe(true);
+        });
+
+        it("called with `false` should return `false`", function () {
+            expect(YJS.Boolean.convert(false)).toBe(false);
+        });
+
+        it("called with `NaN` should return default value", function () {
+            expect(YJS.Boolean.convert(NaN)).toBeNull();
+            expect(YJS.Boolean.convert(NaN, { def: false })).toBe(false);
+            expect(YJS.Boolean.convert(NaN, { def: undefined })).not.toBeDefined();
+        });
+
+        it("called with `Number.NaN` should return default value", function () {
+            expect(YJS.Boolean.convert(Number.NaN)).toBeNull();
+            expect(YJS.Boolean.convert(Number.NaN, { def: false })).toBe(false);
+            expect(YJS.Boolean.convert(Number.NaN, { def: undefined })).not.toBeDefined();
+        });
+
+        it("called with `Number.MAX_VALUE` should return default value", function () {
+            expect(YJS.Boolean.convert(Number.MAX_VALUE)).toBeNull();
+            expect(YJS.Boolean.convert(Number.MAX_VALUE, { def: false })).toBe(false);
+            expect(YJS.Boolean.convert(Number.MAX_VALUE, { def: undefined })).not.toBeDefined();
+        });
+
+        it("called with `Number.MIN_VALUE` should return default value", function () {
+            expect(YJS.Boolean.convert(Number.MIN_VALUE)).toBeNull();
+            expect(YJS.Boolean.convert(Number.MIN_VALUE, { def: false })).toBe(false);
+            expect(YJS.Boolean.convert(Number.MIN_VALUE, { def: undefined })).not.toBeDefined();
+        });
+
+        it("called with `Number.NEGATIVE_INFINITY` should return default value", function () {
+            expect(YJS.Boolean.convert(Number.NEGATIVE_INFINITY)).toBeNull();
+            expect(YJS.Boolean.convert(Number.NEGATIVE_INFINITY, { def: false })).toBe(false);
+            expect(YJS.Boolean.convert(Number.NEGATIVE_INFINITY, { def: undefined })).not.toBeDefined();
+        });
+
+        it("called with `Number.POSITIVE_INFINITY` should return default value", function () {
+            expect(YJS.Boolean.convert(Number.POSITIVE_INFINITY)).toBeNull();
+            expect(YJS.Boolean.convert(Number.POSITIVE_INFINITY, { def: false })).toBe(false);
+            expect(YJS.Boolean.convert(Number.POSITIVE_INFINITY, { def: undefined })).not.toBeDefined();
+        });
+
+        it("called with `undefined` should return default value", function () {
+            expect(YJS.Boolean.convert(undefined)).toBeNull();
+            expect(YJS.Boolean.convert(undefined, { def: false })).toBe(false);
+            expect(YJS.Boolean.convert(undefined, { def: undefined })).not.toBeDefined();
+        });
+
+        it("called with `null` should return default value", function () {
+            expect(YJS.Boolean.convert(null)).toBeNull();
+            expect(YJS.Boolean.convert(null, { def: false })).toBe(false);
+            expect(YJS.Boolean.convert(null, { def: undefined })).not.toBeDefined();
+        });
+
+        it("called with empty string should return default value", function () {
+            expect(YJS.Boolean.convert('')).toBeNull();
+            expect(YJS.Boolean.convert('', { def: false })).toBe(false);
+            expect(YJS.Boolean.convert('', { def: undefined })).not.toBeDefined();
+        });
+
+        it("called with `'null'` should return default value", function () {
+            expect(YJS.Boolean.convert('null')).toBeNull();
+            expect(YJS.Boolean.convert('null', { def: false })).toBe(false);
+            expect(YJS.Boolean.convert('null', { def: undefined })).not.toBeDefined();
+        });
+
+        it("called with `'undefined'` should return default value", function () {
+            expect(YJS.Boolean.convert('undefined')).toBeNull();
+            expect(YJS.Boolean.convert('undefined', { def: false })).toBe(false);
+            expect(YJS.Boolean.convert('undefined', { def: undefined })).not.toBeDefined();
+        });
+
+        it("called with `'0'` should return `false`", function () {
+            expect(YJS.Boolean.convert('0')).toBe(false);
+        });
+
+        it("called with `'1'` should return `true`", function () {
+            expect(YJS.Boolean.convert('1')).toBe(true);
+        });
+
+        it("called with `'f'` should return `false`", function () {
+            expect(YJS.Boolean.convert('f')).toBe(false);
+            expect(YJS.Boolean.convert('F')).toBe(false);
+        });
+
+        it("called with `'false'` should return `false`", function () {
+            expect(YJS.Boolean.convert('false')).toBe(false);
+            expect(YJS.Boolean.convert('falsE')).toBe(false);
+            expect(YJS.Boolean.convert('falSe')).toBe(false);
+            expect(YJS.Boolean.convert('falSE')).toBe(false);
+            expect(YJS.Boolean.convert('faLse')).toBe(false);
+            expect(YJS.Boolean.convert('faLsE')).toBe(false);
+            expect(YJS.Boolean.convert('faLSe')).toBe(false);
+            expect(YJS.Boolean.convert('faLSE')).toBe(false);
+            expect(YJS.Boolean.convert('fAlse')).toBe(false);
+            expect(YJS.Boolean.convert('fAlsE')).toBe(false);
+            expect(YJS.Boolean.convert('fAlSe')).toBe(false);
+            expect(YJS.Boolean.convert('fAlSE')).toBe(false);
+            expect(YJS.Boolean.convert('fALse')).toBe(false);
+            expect(YJS.Boolean.convert('fALsE')).toBe(false);
+            expect(YJS.Boolean.convert('fALSe')).toBe(false);
+            expect(YJS.Boolean.convert('fALSE')).toBe(false);
+            expect(YJS.Boolean.convert('False')).toBe(false);
+            expect(YJS.Boolean.convert('FalsE')).toBe(false);
+            expect(YJS.Boolean.convert('FalSe')).toBe(false);
+            expect(YJS.Boolean.convert('FalSE')).toBe(false);
+            expect(YJS.Boolean.convert('FaLse')).toBe(false);
+            expect(YJS.Boolean.convert('FaLsE')).toBe(false);
+            expect(YJS.Boolean.convert('FaLSe')).toBe(false);
+            expect(YJS.Boolean.convert('FaLSE')).toBe(false);
+            expect(YJS.Boolean.convert('FAlse')).toBe(false);
+            expect(YJS.Boolean.convert('FAlsE')).toBe(false);
+            expect(YJS.Boolean.convert('FAlSe')).toBe(false);
+            expect(YJS.Boolean.convert('FAlSE')).toBe(false);
+            expect(YJS.Boolean.convert('FALse')).toBe(false);
+            expect(YJS.Boolean.convert('FALsE')).toBe(false);
+            expect(YJS.Boolean.convert('FALSe')).toBe(false);
+            expect(YJS.Boolean.convert('FALSE')).toBe(false);
+        });
+
+        it("called with `'no'` should return `false`", function () {
+            expect(YJS.Boolean.convert('no')).toBe(false);
+            expect(YJS.Boolean.convert('nO')).toBe(false);
+            expect(YJS.Boolean.convert('No')).toBe(false);
+            expect(YJS.Boolean.convert('NO')).toBe(false);
+        });
+
+        it("called with `'nope'` should return default value", function () {
+            expect(YJS.Boolean.convert('nope')).toBeNull();
+            expect(YJS.Boolean.convert('nope', { def: false })).toBe(false);
+            expect(YJS.Boolean.convert('nope', { def: undefined })).not.toBeDefined();
+        });
+
+        it("called with `'off'` should return `false`", function () {
+            expect(YJS.Boolean.convert('off')).toBe(false);
+            expect(YJS.Boolean.convert('ofF')).toBe(false);
+            expect(YJS.Boolean.convert('oFf')).toBe(false);
+            expect(YJS.Boolean.convert('oFF')).toBe(false);
+            expect(YJS.Boolean.convert('Off')).toBe(false);
+            expect(YJS.Boolean.convert('OfF')).toBe(false);
+            expect(YJS.Boolean.convert('OFf')).toBe(false);
+            expect(YJS.Boolean.convert('OFF')).toBe(false);
+        });
+
+        it("called with `'on'` should return `true`", function () {
+            expect(YJS.Boolean.convert('on')).toBe(true);
+            expect(YJS.Boolean.convert('oN')).toBe(true);
+            expect(YJS.Boolean.convert('On')).toBe(true);
+            expect(YJS.Boolean.convert('ON')).toBe(true);
+        });
+
+        it("called with `'t'` should return `true`", function () {
+            expect(YJS.Boolean.convert('t')).toBe(true);
+            expect(YJS.Boolean.convert('T')).toBe(true);
+        });
+
+        it("called with `'true'` should return `true`", function () {
+            expect(YJS.Boolean.convert('true')).toBe(true);
+            expect(YJS.Boolean.convert('truE')).toBe(true);
+            expect(YJS.Boolean.convert('trUe')).toBe(true);
+            expect(YJS.Boolean.convert('trUE')).toBe(true);
+            expect(YJS.Boolean.convert('tRue')).toBe(true);
+            expect(YJS.Boolean.convert('tRuE')).toBe(true);
+            expect(YJS.Boolean.convert('tRUe')).toBe(true);
+            expect(YJS.Boolean.convert('tRUE')).toBe(true);
+            expect(YJS.Boolean.convert('True')).toBe(true);
+            expect(YJS.Boolean.convert('TruE')).toBe(true);
+            expect(YJS.Boolean.convert('TrUe')).toBe(true);
+            expect(YJS.Boolean.convert('TrUE')).toBe(true);
+            expect(YJS.Boolean.convert('TRue')).toBe(true);
+            expect(YJS.Boolean.convert('TRuE')).toBe(true);
+            expect(YJS.Boolean.convert('TRUe')).toBe(true);
+            expect(YJS.Boolean.convert('TRUE')).toBe(true);
+        });
+
+        it("called with `'y'` should return `true`", function () {
+            expect(YJS.Boolean.convert('y')).toBe(true);
+            expect(YJS.Boolean.convert('Y')).toBe(true);
+        });
+
+        it("called with `'yes'` should return `true`", function () {
+            expect(YJS.Boolean.convert('yes')).toBe(true);
+            expect(YJS.Boolean.convert('yeS')).toBe(true);
+            expect(YJS.Boolean.convert('yEs')).toBe(true);
+            expect(YJS.Boolean.convert('yES')).toBe(true);
+            expect(YJS.Boolean.convert('Yes')).toBe(true);
+            expect(YJS.Boolean.convert('YeS')).toBe(true);
+            expect(YJS.Boolean.convert('YEs')).toBe(true);
+            expect(YJS.Boolean.convert('YES')).toBe(true);
+        });
+
+        it("called with `-0` should return `false`", function () {
+            expect(YJS.Boolean.convert(-0)).toBe(false);
+        });
+
+        it("called with `0` should return `false`", function () {
+            expect(YJS.Boolean.convert(0)).toBe(false);
+        });
+
+        it("called with `1` should return `true`", function () {
+            expect(YJS.Boolean.convert(1)).toBe(true);
+        });
+
+        it("called with `2` should return default value", function () {
+            expect(YJS.Boolean.convert(2)).toBeNull();
+            expect(YJS.Boolean.convert(2, { def: false })).toBe(false);
+            expect(YJS.Boolean.convert(2, { def: undefined })).not.toBeDefined();
+        });
+
+        it("called with an input that has a `toString` method that returns a falsey value should return `false`", function () {
+            var input;
+            
+            input = { toString: function () { return '0'; } };
+            expect(YJS.Boolean.convert(input)).toBe(false);
+            
+            input = { toString: function () { return 'off'; } };
+            expect(YJS.Boolean.convert(input)).toBe(false);
+            
+            input = { toString: function () { return 'f'; } };
+            expect(YJS.Boolean.convert(input)).toBe(false);
+            
+            input = { toString: function () { return 'false'; } };
+            expect(YJS.Boolean.convert(input)).toBe(false);
+            
+            input = { toString: function () { return 'n'; } };
+            expect(YJS.Boolean.convert(input)).toBe(false);
+            
+            input = { toString: function () { return 'no'; } };
+            expect(YJS.Boolean.convert(input)).toBe(false);
+        });
+
+        it("called with an input that has a `toString` method that returns a truthy value should return `true`", function () {
+            var input;
+            
+            input = { toString: function () { return '1'; } };
+            expect(YJS.Boolean.convert(input)).toBe(true);
+            
+            input = { toString: function () { return 'on'; } };
+            expect(YJS.Boolean.convert(input)).toBe(true);
+            
+            input = { toString: function () { return 't'; } };
+            expect(YJS.Boolean.convert(input)).toBe(true);
+            
+            input = { toString: function () { return 'true'; } };
+            expect(YJS.Boolean.convert(input)).toBe(true);
+            
+            input = { toString: function () { return 'y'; } };
+            expect(YJS.Boolean.convert(input)).toBe(true);
+            
+            input = { toString: function () { return 'yes'; } };
+            expect(YJS.Boolean.convert(input)).toBe(true);
+        });
+
+    });
+
     describe(".not", function () {
 
         it("called with a falsey value and a falsey condition should return `false`", function () {
